5bc029c5e59910f5d77bea20fa3cd738eb85e234
gajendra82 <gajendra.kumar@globalspace.in>
2025-12-15 15:17:34 +0530

    build created

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "577865c2a92339b08503b0bf4cff7bcb
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "22bb140810999aeccb6c06b77dd6e19f",
+"flutter_bootstrap.js": "4d2549d157c87ebd1ce6e2c20234dd17",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
