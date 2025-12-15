0dfe25df488967e3dfbd33b1c7bb0221d34c924a
gajendra82 <gajendra.kumar@globalspace.in>
2025-12-15 13:57:24 +0530

    build created

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -36,4 +36,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c34c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "1565065703"
+  }
+});
