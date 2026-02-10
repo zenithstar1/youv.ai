9cf8d97bb08864e7f4500058c079b017e5576938
Sahil Singh <amnsahil123@gmail.com>
2026-02-10 17:55:25 +0530

    Fixed Login redirecting issue

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,14 +34,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "577865c2a92339b08503b0bf4cff7bcb
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f234cb0aa4f295c2f3ad881766110d3a",
+"flutter_bootstrap.js": "3162aafd067f2a9d7ba5e265cf629a04",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "17dfa3ec80268bc32029a1aecfb08377",
 "/": "17dfa3ec80268bc32029a1aecfb08377",
-"main.dart.js": "d0271c7aad9c2016ab8a0eaeb7f990ab",
+"main.dart.js": "7929db7087463984c708d0ef081bacbc",
 "manifest.json": "ebf24baddec286ab6bf3c458b7ddb3ff",
 "version.json": "c8f38dee5114c2a8de454d88f1b90af8"};
 // The application shell files that are downloaded before a service worker can
