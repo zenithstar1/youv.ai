5802cc238456b7f4dffde465b238e45fcf2f9ece
gajendra82 <gajendra.kumar@globalspace.in>
2026-04-01 22:46:22 +0530

    ui change done

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1806397063"
+    serviceWorkerVersion: "596839419"
   }
 });
