3747bb86828216cbf670089a0f0ea25cfeae3fb1
gajendra82 <gajendra.kumar@globalspace.in>
2026-04-03 17:02:20 +0530

    loading issue r

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4eace50174a5cdd0c262c95118d5a072
 "face_detector.js": "da4d65ec0d68045aec413d7d1f7fc3bd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "252f14d82ca0606c8f35450dbeda82d0",
+"flutter_bootstrap.js": "1167b6a3757102b952058f2aec6d95ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
